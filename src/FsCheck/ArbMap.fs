2d1a9a69823dfeb2798412da09d51adfe4220037
Spencer Farley <2847259+farlee2121@users.noreply.github.com>
2022-07-04 22:57:37 -0500

    Second multi-constructor example. Compiles, failes tests with what looks like improper registration of arbmap as a config parameter

--- src/FsCheck/ArbMap.fs
+++ src/FsCheck/ArbMap.fs
@@ -15,40 +15,36 @@ type IArbMap =
 // not exposed directly, is because that gives annoying type name clashes
 // between the ArbMap type, the ArbMap module (which can be addressed with ModuleSuffix),
 // and the ArbMap extension type in FsCheck.Fluent.
-type internal ArbMap =
+type internal ArbMap private  (tc: TypeClass.TypeClass<Arbitrary<obj>>) as this=
     
-    val finder : TypeClass.TypeClass<Arbitrary<obj>>
-            
-    member private this.ArbFinder = this.finder
+    let finder = tc.Merge(TypeClass.TypeClass(injectedConfigs = [| this |]))
+    member private this.ArbFinder = finder
 
-    internal new (typ:Type, ?existingMap:ArbMap) as this = 
+    internal new (typ:Type, ?existingMap:ArbMap) = 
         let finder =
             match existingMap with
             | None ->
                 TypeClass.TypeClass<Arbitrary<obj>>
                     .New(injectParameters = true)
             | Some arbFinder -> arbFinder.ArbFinder
-            
-        let merged = finder.DiscoverAndMerge(onlyPublic = false, instancesType = typ, newInjectedConfigs = [| this |])
-        {finder = merged}
-
-    internal new (tc: TypeClass.TypeClass<Arbitrary<obj>>) as this=
-        {finder = tc.Merge(TypeClass.TypeClass(injectedConfigs = [| this |]))}
+        
+        let merged = finder.DiscoverAndMerge(onlyPublic = false, instancesType = typ)
+        ArbMap(merged)
 
     // for testing purposes
-    member internal this.MemoizedInstances = this.finder.MemoizedInstances
+    member internal this.MemoizedInstances = this.ArbFinder.MemoizedInstances
 
     member internal this.MergeFactory(factory: Func<'a,Arbitrary<'b>>) =
-        ArbMap(this.finder.MergeFactory(factory)) 
+        ArbMap(this.ArbFinder.MergeFactory(factory)) 
 
     member internal this.MergeFactory(target: obj, method: System.Reflection.MethodInfo) =
-        ArbMap(this.finder.MergeFactory(target,method))
+        ArbMap(this.ArbFinder.MergeFactory(target,method))
 
     interface IArbMap with
         member this.ArbFor t =
-            this.finder.GetInstance t
+            this.ArbFinder.GetInstance t
             |> unbox<IArbitrary>
             |> (fun arb -> Arb.fromGenShrink (arb.GeneratorObj, arb.ShrinkerObj))
 
         member this.ArbFor<'TArb>() =
-            this.finder.InstanceFor<'TArb, Arbitrary<'TArb>>()
+            this.ArbFinder.InstanceFor<'TArb, Arbitrary<'TArb>>()
